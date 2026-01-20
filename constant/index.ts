c22c859ded2463e7b777befa70d0bc0f3d9ffc79
Luke <pmduc1997@gmail.com>
2026-01-20 22:47:26 +0700

    update internet bill

--- constant/index.ts
+++ constant/index.ts
@@ -19,7 +19,7 @@ export const DEFAULT_ROOM_INFO = {
   price: 4000000,
   elec: { start: 0, end: 0, used: 0, price: 4200 },
   water: { start: 0, end: 0, used: 0, price: 35000 },
-  services: { cleaning: 100000, person: 1, washing: 50000, internet: 50000 },
+  services: { cleaning: 100000, person: 1, washing: 50000, internet: 100000 },
 };
 
 export const BANK_INFO = {
